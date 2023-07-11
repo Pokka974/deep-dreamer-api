7792c217af4506fb3a801e6654dbea28f6f0e988
Julien <deji97410@gmail.com>
2023-07-11 21:25:00 +0200

    optimized route

--- src/services/chatgpt.service.ts
+++ src/services/chatgpt.service.ts
@@ -6,10 +6,17 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+const englishPrompt =
+    'Given that dream interpretation is highly subjective and can depend on a variety of factors, provide a detailed interpretation based on common symbols and themes for the following dream:';
+const frenchPrompt =
+    "Étant donné que l'interprétation des rêves est hautement subjective et peut dépendre de divers facteurs, fournissez une interprétation détaillée basée sur les symboles et thèmes communs pour le rêve suivant :";
 
 const getCompletion = async (userInput: string, maxTokens: number) => {
-    const prompt = `Given that dream interpretation is highly subjective and can depend on a variety of factors, provide a detailed interpretation based on common symbols and themes for the following dream: ${userInput}`;
-    console.log(prompt);
+    if (!userInput) {
+        return ''; // Return empty string if userInput is empty
+    }
+
+    const prompt = `${frenchPrompt} ${userInput}`;
 
     const gptResponse = await openai.createCompletion({
         model: 'text-davinci-003',
@@ -21,7 +28,11 @@ const getCompletion = async (userInput: string, maxTokens: number) => {
         presence_penalty: 0.1,
     });
 
-    return gptResponse.data.choices[0].text;
+    const responseText: string | undefined = gptResponse.data.choices[0].text;
+    const interpretation = responseText
+        ?.substring(responseText.indexOf('\n') + 1)
+        .trim();
+    return interpretation;
 };
 
 export default {
